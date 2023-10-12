012b3b3047771ecc6972ed4ab08e0f009713361b
ytmtravels <147376808+ytmtravels@users.noreply.github.com>
2023-10-12 16:49:13 +0600

    Update route.js

--- app/api/search-memory/route.js
+++ app/api/search-memory/route.js
@@ -2,41 +2,21 @@ import { connectMongoDB } from "@/lib/mongodb";
 import myMemories from "@/models/myMemories";
 import { NextResponse } from "next/server";
 
-export async function GET(req) {
+export async function POST(req) {
   await connectMongoDB();
   try {
-    const { searchParams } = new URL(req.url);
-    const userID = searchParams.get("userId");
-    const query = searchParams.get("q");
-    console.log("memory by user id: ", userID);
-    console.log("memory by user query: ", query);
+    const { userId, search } = await req.json();
+    console.log("searchData: ", userId);
+    console.log("searchData: ", search);
+
+    const query = search;
     const memories = await myMemories.find({
-      userId: userID,
+      userId: userId,
       $or: [
         { address: { $regex: query, $options: "i" } },
         { full_address: { $regex: query, $options: "i" } },
       ],
     });
-
-    // const uniqueCountries = await myMemories
-    //   .find({ userId: userID })
-    //   .distinct("country");
-
-    // const CountryCount = uniqueCountries.length;
-
-    // const uniqueCity = await myMemories
-    //   .find({ userId: userID })
-    //   .distinct("city");
-
-    // const cityCount = uniqueCity.length;
-
-    // console.log("server myMemories count: ", {
-    //   CountryCount,
-    //   uniqueCountries,
-    //   cityCount,
-    //   uniqueCity,
-    // });
-    // console.log("server myMemories: ", memories);
     return NextResponse.json({ memories });
   } catch (error) {
     console.log("error: ", error);
